96df6a76540e923909c60eb3999967337226466a
stackbaxter <stackbaxter@yahoo.com>
2017-08-04 02:23:33 -0500

    need to pass the hide/show on down

--- src/components/Review.js
+++ src/components/Review.js
@@ -3,36 +3,37 @@ import StarRating from "./StarRating";
 export default class Review extends React.Component {
   constructor(props){
     super(props);
-    this.setState({
+    this.state = {
       visible: false
-    })
+    }
     this.onClick = this.onClick.bind(this);
   }
   onClick(e) {
-    e.preventDefault();
     let rev = document.getElementById('reviews');
 
-    this.state.visible ? rev.style.display = 'none' : rev.style.display = 'block';
+    this.state.visible ? rev.style.display = 'none' : rev.style.display = 'inline';
     this.setState({ visible: !this.state.visible})
      }
 
   render() {
+    
     let revNum = this.props.reviews.length > 1 ? 'reviews' : 'review';
-
-  let reviews = this.props.reviews.map(x => {
-      return (
-        <div key={this.props.keys}>
-          <p>{x.description}</p>
-          <StarRating stars={x.rating} />
+    let display = {
+      display: 'none'
+    }
+      let reviews = this.props.reviews.map((x,i) => {
+          return (
+            <div key={i}>
+              <p>{x.description}</p>
+              <StarRating stars={x.rating} />
+            </div>
+          );
+        })
+      return(
+        <div className="indReviews">
+          <StarRating stars={this.props.rating} /> <p onClick={this.onClick}>{this.props.reviews.length} {revNum}</p>
+          <div id='reviews' style={display}>{reviews}</div>
         </div>
-      );
-    })
-
-    return(
-      <div>
-        <StarRating stars={this.props.rating} /> <p onClick={(e) => this.onClick}>{revNum}</p>
-        <div id='reviews'>{reviews}</div>
-      </div>
 
     )
 
